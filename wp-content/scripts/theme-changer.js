fe566e9c49ef3c0bc652e6c2a35a493ae9e7f6e6
dsatpm <67771-dsatpm@users.noreply.drupalcode.org>
2024-10-08 17:33:23 -0700

    second commit

---
+++ wp-content/scripts/theme-changer.js
@@ -0,0 +1,7 @@
+function changeTheme() {
+    let theme = document.getElementById('theme');
+    let purpleTheme = 'wp-content/stylesheets/purple-theme.css';
+    let grayTheme = 'wp-content/stylesheets/gray-theme.css';
+    let greenTheme = 'wp-content/stylesheets/green-theme.css';
+    
+}
